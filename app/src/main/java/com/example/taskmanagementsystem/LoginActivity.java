c0cdca7aee16c46d0a435152bbe5b6e2c7428428
Muadz <muadzzinzahir@gmail.com>
2026-01-03 23:03:35 +0800

    Edit login wan to farah

--- app/src/main/java/com/example/taskmanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/taskmanagementsystem/LoginActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    String[] employees = {"Azim","Ilham","Hanif","Syera","Wann","Hakim","Danial","Nadia"};
+    String[] employees = {"Azim","Ilham","Hanif","Syera","Farah","Hakim","Danial","Nadia"};
     String manager = "Manager";
 
     @Override
