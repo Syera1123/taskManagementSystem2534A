5713734b2e6179a4771d03fe6c17b681aea4622b
HANIF <hanifnazmiboi@gmail.com>
2026-01-06 09:06:54 +0800

    Updated Aptitude Link and added Update Status button in Employee Task List

--- app/src/main/java/com/example/taskmanagementsystem/remote/ApiUtils.java
+++ app/src/main/java/com/example/taskmanagementsystem/remote/ApiUtils.java
@@ -1,7 +1,7 @@
 package com.example.taskmanagementsystem.remote;
 
 public class ApiUtils {
-    public static final String BASE_URL = "https://aptitude.my/2024973881/api/";
+    public static final String BASE_URL = "https://aptitude.my/taskmanagement/api/";
 
     public static UserService getUserService() {
         return RetrofitClient.getClient(BASE_URL).create(UserService.class);
